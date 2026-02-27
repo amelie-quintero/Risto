ce1cdd8305ac3bc807c7f7c5bbee2bfab4010b63
amelie-quintero <224878763+amelie-quintero@users.noreply.github.com>
2026-02-27 15:48:47 -0500

    initial commit

---
+++ src/main/java/com/example/Risto/RistoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Risto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RistoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RistoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Risto/RistoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Risto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RistoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
