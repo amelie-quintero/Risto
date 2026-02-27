84720234664e305be2d0178ae36b3c1fd9a74cb4
amelie-quintero <224878763+amelie-quintero@users.noreply.github.com>
2026-02-27 16:34:41 -0500

    Created admin entity

---
+++ src/main/java/com/example/Risto/entities/Admin.java
@@ -0,0 +1,31 @@
+package com.example.Risto.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "admin")
+public class Admin {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@NotNull
+	private String username;
+	
+	@NotNull
+	@Email
+	private String email;
+	
+	@NotNull
+	private String password;
+}
