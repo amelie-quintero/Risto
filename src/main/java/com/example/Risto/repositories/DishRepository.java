2ff7ce3a5e7c3eb8987554b4cda2a4b23c270e98
amelie-quintero <224878763+amelie-quintero@users.noreply.github.com>
2026-03-03 00:01:21 -0500

    added price search methods

--- src/main/java/com/example/Risto/repositories/DishRepository.java
+++ src/main/java/com/example/Risto/repositories/DishRepository.java
@@ -1,10 +1,14 @@
 package com.example.Risto.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.Risto.entities.Dish;
 
 public interface DishRepository extends CrudRepository<Dish, Integer> {
 
-	public Dish findDishByName(String name);
+	public Dish findByName(String name);
+	public List<Dish> findByPriceLessThan(Double price);
+	public List<Dish> findByPriceGreaterThan(Double price);
 }
