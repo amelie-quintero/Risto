d14035ca289ec58ae2f7537a9d80ea799520d1aa
amelie-quintero <224878763+amelie-quintero@users.noreply.github.com>
2026-03-03 16:46:55 -0500

    added new @Query annotations and reworked queries

--- src/main/java/com/example/Risto/repositories/IngredientRepository.java
+++ src/main/java/com/example/Risto/repositories/IngredientRepository.java
@@ -1,13 +1,19 @@
 package com.example.Risto.repositories;
 
-import java.util.Set;
-
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.Risto.entities.Ingredient;
 
 public interface IngredientRepository extends CrudRepository<Ingredient, Integer> {
 
-	public Ingredient findIngredientByName(String name);
-	public Set<Ingredient> findLowIngredients();
+	public Optional<Ingredient> findByName(String name);
+	
+	@Query("select i from i where i.name like %?1%")
+	public List<Ingredient> findByNameLike(String search);
+	
+	@Query("select i from i where i.amount < i.lowThreshold")
+	public List<Ingredient> findLowIngredients();
 }
