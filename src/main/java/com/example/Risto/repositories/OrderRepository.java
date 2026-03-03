30dc13fb7650011700b49ce7c3bd681ed4fdefe0
amelie-quintero <224878763+amelie-quintero@users.noreply.github.com>
2026-03-03 00:02:57 -0500

    added ability to search between dates and by id

--- src/main/java/com/example/Risto/repositories/OrderRepository.java
+++ src/main/java/com/example/Risto/repositories/OrderRepository.java
@@ -1,10 +1,14 @@
 package com.example.Risto.repositories;
 
+import java.sql.Date;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.Risto.entities.Order;
 
 public interface OrderRepository extends CrudRepository<Order, Integer> {
 
-	
+	public List<Order> findByUserId(int userId);
+	public List<Order> findByDateBetween(Date startDate, Date endDate);
 }
