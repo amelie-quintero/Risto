d3d7997ea99d550d551debfc791489351d955735
amelie-quintero <224878763+amelie-quintero@users.noreply.github.com>
2026-03-02 23:13:31 -0500

    Optimized imports, reworked interface methods to use JPA query methods

--- src/main/java/com/example/Risto/repositories/UserRepository.java
+++ src/main/java/com/example/Risto/repositories/UserRepository.java
@@ -1,14 +1,14 @@
 package com.example.Risto.repositories;
 
-import java.util.Set;
-
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.Risto.entities.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 
-	public User findUserByEmail(String email);
-	public User findUserByName(String name);
-	public Set<User> getActiveUsers();
+	public Optional<User> findByEmail(String email);
+	public Optional<User> findByUsername(String name);
+	public List<User> findByActive(boolean active);
 }
