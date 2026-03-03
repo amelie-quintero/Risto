4a034a7fdd9a1dea8899866a857b79309683d895
amelie-quintero <224878763+amelie-quintero@users.noreply.github.com>
2026-03-03 17:46:09 -0500

    created MissingIngredientException class

---
+++ src/main/java/com/example/Risto/exceptions/MissingIngredientException.java
@@ -0,0 +1,19 @@
+package com.example.Risto.exceptions;
+
+import java.util.List;
+
+import com.example.Risto.constants.Messages;
+
+public class MissingIngredientException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	public MissingIngredientException(String message) {
+		super(message);
+	}
+	
+	public MissingIngredientException(List<String> missing) {
+		String message = Messages.Errors.MISSINGINGREDIENTEXCEPTIONMSG + String.join(", ", missing);
+		new MissingIngredientException(message);
+	}
+}
