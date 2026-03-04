0b4eb5f4645585aebd69f1da1a4693da79279030
Jude Quintero <quinterojude@gmail.com>
2026-03-04 17:54:09 -0500

    added defined query for findByNameLike method

--- src/main/java/com/example/Risto/repositories/DishRepository.java
+++ src/main/java/com/example/Risto/repositories/DishRepository.java
@@ -3,6 +3,7 @@ package com.example.Risto.repositories;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.Risto.entities.Dish;
@@ -11,5 +12,7 @@ public interface DishRepository extends CrudRepository<Dish, Integer> {
 
 	public Optional<Dish> findByName(String name);
 	public List<Dish> findByPriceBetween(Double lowerBound, Double upperBound);
+	
+	@Query("select d from d where d.name like %?1%")
 	public List<Dish> findByNameLike(String search);
 }
