63f9bc26833ead89e42c9dbd3c75eb54057ada34
Jude Quintero <quinterojude@gmail.com>
2026-03-04 15:55:54 -0500

    added DishTests file

---
+++ src/test/java/com/example/Risto/entities/DishTests.java
@@ -0,0 +1,47 @@
+package com.example.Risto.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DishTests {
+
+	private Dish dish;
+	
+	@BeforeEach
+	void setup() {
+		this.dish = new Dish();
+	}
+	
+	@Test
+	@DisplayName(value = "Test that the dish id field is correctly assigned when the object is created")
+	void testId() {
+		assertNotNull(this.dish.getId());
+	}
+	
+	@Test
+	@DisplayName(value = "Test that the name field can be written and read correctly")
+	void testName() {
+		this.dish.setName("test");
+		assertEquals("test", this.dish.getName());
+		
+		this.dish.setName(null);
+		assertNull(this.dish.getName());
+	}
+	
+	@Test
+	@DisplayName(value = "Test that the price field can be written and read correctly")
+	void testPrice() {
+		this.dish.setPrice(Double.valueOf(10.0));
+		assertEquals(Double.valueOf(10.00), this.dish.getPrice());
+		
+		this.dish.setPrice(null);
+		assertNull(this.dish.getPrice());
+	}
+	
+	//TODO ADD VALIDATION TESTS
+}
