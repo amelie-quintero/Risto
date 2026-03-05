e08917c97a1f3d3c037a19a98604a80cf06e87a5
amelie-quintero <224878763+amelie-quintero@users.noreply.github.com>
2026-03-05 18:42:36 -0500

    created orderDishRepository file

---
+++ src/main/java/com/example/Risto/repositories/OrderDishRepository.java
@@ -0,0 +1,9 @@
+package com.example.Risto.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.Risto.entities.OrderDish;
+
+public interface OrderDishRepository extends CrudRepository<OrderDish, Integer> {
+
+}
