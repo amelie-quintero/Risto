81f4d6efe1f8cf57f69d2d4d6e965ea1b3b20799
amelie-quintero <224878763+amelie-quintero@users.noreply.github.com>
2026-03-05 18:00:37 -0500

    updated DishIngredientTests with new field and column changes

--- src/test/java/com/example/Risto/entities/DishIngredientTests.java
+++ src/test/java/com/example/Risto/entities/DishIngredientTests.java
@@ -1,46 +1,76 @@
 package com.example.Risto.entities;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 
+import com.example.Risto.repositories.DishIngredientRepository;
+import com.example.Risto.repositories.DishRepository;
+import com.example.Risto.repositories.IngredientRepository;
+
+@DataJpaTest
+@TestInstance(Lifecycle.PER_CLASS)
 public class DishIngredientTests {
-
-	private DishIngredient dishIngredient;
+	
+	@Autowired
+	private DishRepository dishStore;
+	
+	@Autowired
+	private IngredientRepository ingredientStore;
+	
+	@Autowired
+	private DishIngredientRepository diStore;
+	
+	private Dish dish1, dish2;
+	
+	private Ingredient ingredient1, ingredient2;
 	
 	@BeforeEach
 	void setup() {
-		this.dishIngredient = DishIngredient.builder().build();
+		dish1 = dishStore.save(Dish.builder().name("dish1").price(10.0).build());
+		dish2 = dishStore.save(Dish.builder().name("dish2").price(12.0).build());
+		ingredient1 = ingredientStore.save(Ingredient.builder().name("ing1").amount(100.0).lowThreshold(10.0).build());
+		ingredient2 = ingredientStore.save(Ingredient.builder().name("ing2").amount(100.0).lowThreshold(10.0).build());
 	}
 	
 	@Test
 	@DisplayName(value = "Test that the id field is populated correctly on object creation")
 	void testId() {
-		assertNotNull(this.dishIngredient.getId());
+		DishIngredient di = diStore.save(DishIngredient.builder().dish(dish1).ingredient(ingredient1).amount(10.0).build());
+		assertNotEquals(0, di.getId());
 	}
 	
 	@Test
-	@DisplayName(value = "Test that the dishId field is written and read correctly")
-	void testDishId() {
-		this.dishIngredient.setDishId(1234);
-		assertEquals(1234, this.dishIngredient.getDishId());
-		
-		this.dishIngredient.setDishId(0);
-		assertEquals(0, this.dishIngredient.getDishId());
+	@DisplayName(value = "Test that the amount field is written and read correctly")
+	void testAmount() {
+		DishIngredient di = diStore.save(DishIngredient.builder().dish(dish1).ingredient(ingredient1).amount(10.0).build());
+		di.setAmount(15.0);
+		DishIngredient di1 = diStore.save(di);
+		assertEquals(15.0, di1.getAmount());
 	}
 	
 	@Test
-	@DisplayName(value = "Test that the ingredientId field is written and read correctly")
-	void testIngredientId() {
-		this.dishIngredient.setIngredientId(1234);
-		assertEquals(1234, this.dishIngredient.getIngredientId());
-		
-		this.dishIngredient.setIngredientId(0);
-		assertEquals(0, this.dishIngredient.getIngredientId());
+	@DisplayName(value = "Test that the Dish value is written correctly and the dish_id column in the db is correct")
+	void testDish() {
+		DishIngredient di = diStore.save(DishIngredient.builder().dish(dish1).ingredient(ingredient1).amount(10.0).build());
+		di.setDish(dish2);
+		DishIngredient di1 = diStore.save(di);
+		assertEquals(dish2, di1.getDish());
 	}
 	
+	@Test
+	@DisplayName(value = "Test that the Ingredient value is written correctly and the ingredient_id column in the db is correct")
+	void testIngredient() {
+		DishIngredient di = diStore.save(DishIngredient.builder().dish(dish1).ingredient(ingredient1).amount(10.0).build());
+		di.setIngredient(ingredient2);
+		DishIngredient di1 = diStore.save(di);
+		assertEquals(ingredient2, di1.getIngredient());
+	}
 //	TODO ADD VALIDATION TEST CASES
 }
