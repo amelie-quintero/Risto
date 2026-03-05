5576006f55973283d8809f6a11108ad4c8bba795
amelie-quintero <224878763+amelie-quintero@users.noreply.github.com>
2026-03-05 18:54:09 -0500

    Updated OrderTests to correctly test with new methods and fields

--- src/test/java/com/example/Risto/entities/OrderTests.java
+++ src/test/java/com/example/Risto/entities/OrderTests.java
@@ -1,46 +1,67 @@
 package com.example.Risto.entities;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import java.sql.Date;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jpa.test.autoconfigure.TestEntityManager;
 
+import com.example.Risto.constants.OrderStatus;
+
+@DataJpaTest
+@TestInstance(Lifecycle.PER_CLASS)
 public class OrderTests {
 
-	private Order order;
+	@Autowired
+	private TestEntityManager entityManager;
+	
+	private User user1, user2;
+	
+	private Order o1, o2;
 	
 	@BeforeEach
 	void setup() {
-		this.order = Order.builder().build();
+		user1 = entityManager.persist(User.builder().active(true).email("user1@example.com").username("user1").password("pw1").build());
+		user2 = entityManager.persist(User.builder().active(true).email("user2@example.com").username("user2").password("pw2").build());
+		o1 = entityManager.persistAndFlush(Order.builder().date(Date.valueOf("2000-12-12")).status(OrderStatus.PENDING).user(user1).build());
 	}
 	
 	@Test
 	@DisplayName(value = "Test that the id field is correctly populated on object creation")
 	void testId() {
-		assertNotNull(this.order.getId());
-	}
-	
-	@Test
-	@DisplayName(value = "Test that the userId field is correctly written and read")
-	void testUserId() {
-		this.order.setUserId(1234);
-		assertEquals(1234, this.order.getUserId());
-		
-		this.order.setUserId(0);
-		assertEquals(0, this.order.getUserId());
+		assertNotEquals(0, o1.getId());
 	}
 	
 	@Test
 	@DisplayName(value = "Test that the date field is correctly written and read")
 	void testDate() {
 		Date date = Date.valueOf("2000-12-31");
-		this.order.setDate(date);
-		assertEquals("2000-12-31", this.order.getDate().toString());
+		o1.setDate(date);
+		o2 = entityManager.persistAndFlush(o1);
+		assertEquals(date, o2.getDate());
 	}
 	
-//	TODO ADD VALIDATION TEST CASES
+	@Test
+	@DisplayName(value = "Test that the status field is correctly written and read")
+	void testStatus() {
+		o1.setStatus(OrderStatus.INPROGRESS);
+		o2 = entityManager.persistAndFlush(o1);
+		assertEquals(OrderStatus.INPROGRESS, o2.getStatus());
+	}
+	
+	@Test
+	@DisplayName(value = "Test that the User field can be correctly written and set")
+	void testUser() {
+		o1.setUser(user2);
+		o2 = entityManager.persistAndFlush(o1);
+		assertEquals(user2, o2.getUser());
+	}
 }
