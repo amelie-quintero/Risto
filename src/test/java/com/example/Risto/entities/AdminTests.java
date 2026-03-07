f8f51116542faeff7b4b8bbd331abf6eb06f4d0b
amelie-quintero <224878763+amelie-quintero@users.noreply.github.com>
2026-03-06 23:09:43 -0500

    updated AdminTests with auditor

--- src/test/java/com/example/Risto/entities/AdminTests.java
+++ src/test/java/com/example/Risto/entities/AdminTests.java
@@ -8,6 +8,15 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jpa.test.autoconfigure.TestEntityManager;
+import org.springframework.context.annotation.Import;
+
+import com.example.Risto.config.TestAuditorConfig;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 
@@ -16,67 +25,86 @@ import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
 
+@DataJpaTest
+@TestInstance(Lifecycle.PER_CLASS)
+@Import(TestAuditorConfig.class)
 public class AdminTests {
 	
-	private Admin admin;
+	@Autowired
+	private TestEntityManager entityManager;
+	
+	private Admin admin1, admin2;
 	
 	@BeforeEach
 	void setup() {
-		this.admin = Admin.builder().build();
+		this.admin1 = entityManager.persistAndFlush(Admin.builder().active(true).email("admin@example.com").username("admin").password("pw").build());
 	}
 
 	@Test
 	@DisplayName(value = "Test that an Admin Id is correctly assigned when the object is created")
 	void testId() {
-		assertNotNull(this.admin.getId());
+		assertNotNull(this.admin1.getId());
+	}
+	
+	@Test
+	@DisplayName(value = "Test that the created_at and updated_at fields are set correctly by the auditor")
+	void testAuditing() {
+		assertNotNull(admin1.getCreatedAt());
+		assertNotNull(admin1.getUpdatedAt());
+		assertEquals(admin1.getCreatedAt(), admin1.getUpdatedAt());
+		
+		admin1.setActive(false);
+		admin2 = entityManager.persistAndFlush(admin1);
+		assertTrue(admin2.getUpdatedAt().isAfter(admin2.getCreatedAt()));
 	}
 	
 	@Test
 	@DisplayName(value = "Test that the Admin Username is correctly set and retrieved")
 	void testUsername() {
-		this.admin.setUsername("test");
-		assertEquals("test", this.admin.getUsername());
+		this.admin1.setUsername("test");
+		admin2 = entityManager.persistAndFlush(admin1);
+		assertEquals("test", this.admin2.getUsername());
 	}
 	
 	@Test
 	@DisplayName(value = "Test that the Admin Password is correctly set and retrieved")
 	void testPassword() {
-		this.admin.setPassword("test password");
-		assertEquals("test password", this.admin.getPassword());
+		this.admin1.setPassword("test password");
+		admin2 = entityManager.persistAndFlush(admin1);
+		assertEquals("test password", this.admin2.getPassword());
 	}
 	
 	@Test
 	@DisplayName(value = "Test that the Admin Email is correctly set and retrieved when valid")
 	void testEmail() {
-		this.admin.setEmail("email@example.com");
-		assertEquals("email@example.com", this.admin.getEmail());
+		this.admin1.setEmail("email@example.com");
+		admin2 = entityManager.persistAndFlush(admin1);
+		assertEquals("email@example.com", this.admin2.getEmail());
 	}
 	
 	@Test
-	@DisplayName(value = "Test that an invalid Admin Email will throw a ConstraintViolation Exception")
+	@DisplayName(value = "Test that a null Admin Username will throw a ConstraintViolation Exception")
 	void testInvalidUsername() {
 		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
 		Validator validator = factory.getValidator();
 		
-		this.admin.setPassword("test");
-		this.admin.setEmail("email@example.com");
+		admin1.setUsername(null);
 		
-		ArrayList<ConstraintViolation<Admin>> violations = new ArrayList<>(validator.validate(this.admin));
+		ArrayList<ConstraintViolation<Admin>> violations = new ArrayList<>(validator.validate(this.admin1));
 		
 		assertEquals(1, violations.size());
 		assertEquals("must not be null", violations.get(0).getMessage());
 	}
 	
 	@Test
-	@DisplayName(value = "Test that an invalid Admin Email will throw a ConstraintViolation Exception")
+	@DisplayName(value = "Test that a null Admin Email will throw a ConstraintViolation Exception")
 	void testInvalidPassword() {
 		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
 		Validator validator = factory.getValidator();
 		
-		this.admin.setUsername("test");
-		this.admin.setEmail("email@example.com");
+		admin1.setEmail(null);
 		
-		ArrayList<ConstraintViolation<Admin>> violations = new ArrayList<>(validator.validate(this.admin));
+		ArrayList<ConstraintViolation<Admin>> violations = new ArrayList<>(validator.validate(this.admin1));
 		
 		assertEquals(1, violations.size());
 		assertEquals("must not be null", violations.get(0).getMessage());
@@ -88,11 +116,9 @@ public class AdminTests {
 		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
 		Validator validator = factory.getValidator();
 		
-		this.admin.setPassword("test");
-		this.admin.setUsername("test");
-		this.admin.setEmail("randominvalidvalue");
+		this.admin1.setEmail("randominvalidvalue");
 		
-		ArrayList<ConstraintViolation<Admin>> violations = new ArrayList<>(validator.validate(this.admin));
+		ArrayList<ConstraintViolation<Admin>> violations = new ArrayList<>(validator.validate(this.admin1));
 		
 		assertEquals(1, violations.size());
 		assertEquals("must be a well-formed email address", violations.get(0).getMessage());
@@ -101,13 +127,16 @@ public class AdminTests {
 	@Test
 	@DisplayName(value = "Test for active field")
 	void testAdminfield() {
-		this.admin.setActive(false);
-		assertFalse(this.admin.getActive());
+		this.admin1.setActive(false);
+		admin2 = entityManager.persistAndFlush(admin1);
+		assertFalse(this.admin2.getActive());
 		
-		this.admin.setActive(true);
-		assertTrue(this.admin.getActive());
+		this.admin1.setActive(true);
+		admin2 = entityManager.persistAndFlush(admin1);
+		assertTrue(this.admin2.getActive());
 		
-		this.admin.setActive(null);
-		assertNull(this.admin.getActive());
+		this.admin1.setActive(null);
+		admin2 = entityManager.persistAndFlush(admin1);
+		assertNull(this.admin2.getActive());
 	}
 }
