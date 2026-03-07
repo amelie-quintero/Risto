46647245458092df148d59871885ad136348f7d2
amelie-quintero <224878763+amelie-quintero@users.noreply.github.com>
2026-03-06 22:43:45 -0500

    updated OrderTests to include auditing

--- src/test/java/com/example/Risto/entities/OrderTests.java
+++ src/test/java/com/example/Risto/entities/OrderTests.java
@@ -1,7 +1,9 @@
 package com.example.Risto.entities;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.sql.Date;
 
@@ -13,11 +15,13 @@ import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.boot.jpa.test.autoconfigure.TestEntityManager;
-
+import org.springframework.context.annotation.Import;
+import com.example.Risto.config.TestAuditorConfig;
 import com.example.Risto.constants.OrderStatus;
 
 @DataJpaTest
 @TestInstance(Lifecycle.PER_CLASS)
+@Import(TestAuditorConfig.class)
 public class OrderTests {
 
 	@Autowired
@@ -38,6 +42,18 @@ public class OrderTests {
 	@DisplayName(value = "Test that the id field is correctly populated on object creation")
 	void testId() {
 		assertNotEquals(0, o1.getId());
+	}
+	
+	@Test
+	@DisplayName(value = "Test that the created_at and updated_at fields are being set properly by auditor")
+	void testAuditing() {
+		assertNotNull(o1.getCreatedAt());
+		assertNotNull(o1.getUpdatedAt());
+		assertEquals(o1.getCreatedAt(), o1.getUpdatedAt());
+		
+		o1.setStatus(OrderStatus.INPROGRESS);
+		Order o2 = entityManager.persistAndFlush(o1);
+		assertTrue(o2.getUpdatedAt().isAfter(o2.getCreatedAt()));
 	}
 	
 	@Test
