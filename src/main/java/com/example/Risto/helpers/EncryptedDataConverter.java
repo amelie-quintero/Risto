967681939953fbb2a1d587f53514ae5b728cffe5
amelie-quintero <224878763+amelie-quintero@users.noreply.github.com>
2026-03-11 13:59:08 -0400

    Updated EncryptedDataConverter to pull key from application.properties

--- src/main/java/com/example/Risto/helpers/EncryptedDataConverter.java
+++ src/main/java/com/example/Risto/helpers/EncryptedDataConverter.java
@@ -1,15 +1,28 @@
 package com.example.Risto.helpers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
 import com.example.Risto.util.AESUtil;
 
 import jakarta.persistence.AttributeConverter;
 
+@Component
 public class EncryptedDataConverter implements AttributeConverter<String, String> {
 
+	private static Environment environment;
+	
+	@Autowired
+	public void setEnvironment(Environment environment) {
+		EncryptedDataConverter.environment = environment;
+	}
+	
 	@Override
 	public String convertToDatabaseColumn(String attribute) {
 		try {
-			return AESUtil.encryptText(attribute);
+			String SECRET_KEY = environment.getProperty("encrypt.SECRET_KEY");
+			return AESUtil.encryptText(SECRET_KEY, attribute);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
@@ -19,11 +32,11 @@ public class EncryptedDataConverter implements AttributeConverter<String, String
 	@Override
 	public String convertToEntityAttribute(String dbData) {
 		try {
-			return AESUtil.decryptText(dbData);
+			String SECRET_KEY = environment.getProperty("encrypt.SECRET_KEY");
+			return AESUtil.decryptText(SECRET_KEY, dbData);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
-
 }
