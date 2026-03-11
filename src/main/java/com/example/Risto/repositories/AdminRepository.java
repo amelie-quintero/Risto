86a0433f0483beaedfc7038a3207e39145ff159a
amelie-quintero <224878763+amelie-quintero@users.noreply.github.com>
2026-03-11 14:00:02 -0400

    removed findbyemail method, will implement later if needed

--- src/main/java/com/example/Risto/repositories/AdminRepository.java
+++ src/main/java/com/example/Risto/repositories/AdminRepository.java
@@ -2,13 +2,14 @@ package com.example.Risto.repositories;
 
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.Risto.entities.Admin;
 
 public interface AdminRepository extends CrudRepository<Admin, Integer> {
 	
-	public Optional<Admin> findByEmail(String email);
 	public Optional<Admin> findByUsername(String username);
+
 	public List<Admin> findByActiveTrue();
 }
