74fab29a5be5b17cd883303de47762e784c993ab
amelie-quintero <224878763+amelie-quintero@users.noreply.github.com>
2026-03-11 16:05:50 -0400

    added active user check with order creation

--- src/main/java/com/example/Risto/controller/OrderController.java
+++ src/main/java/com/example/Risto/controller/OrderController.java
@@ -1,22 +1,35 @@
 package com.example.Risto.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Risto.dto.PlaceOrderRequestDTO;
 import com.example.Risto.dto.PlaceOrderResponseDTO;
+import com.example.Risto.services.OrderService;
+import com.example.Risto.services.UserService;
 
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/order")
 public class OrderController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private OrderService orderService;
 
 	@PostMapping("/place")
 	public PlaceOrderResponseDTO placeOrder(@Valid PlaceOrderRequestDTO data) {
-		
-		
-		return PlaceOrderResponseDTO.builder().success(false).build();
+		String message = "";
+		if (userService.isActiveUser(data.getUserId())) {
+			
+		} else {
+			message = "Not an active user.";
+		}
+		return PlaceOrderResponseDTO.builder().success(false).message(message).build();
 	}
 }
