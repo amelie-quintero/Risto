7b10a7b424a1bce6efa2b68a41690cb0fd86666d
amelie-quintero <224878763+amelie-quintero@users.noreply.github.com>
2026-03-11 14:42:07 -0400

    added admin login search for admin login function

--- src/main/java/com/example/Risto/repositories/AdminRepository.java
+++ src/main/java/com/example/Risto/repositories/AdminRepository.java
@@ -3,6 +3,7 @@ package com.example.Risto.repositories;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.Risto.entities.Admin;
@@ -12,4 +13,7 @@ public interface AdminRepository extends CrudRepository<Admin, Integer> {
 	public Optional<Admin> findByUsername(String username);
 
 	public List<Admin> findByActiveTrue();
+	
+	@Query("select A from admin A where A.username=?1 and A.password=?2 and A.active=true")
+	public List<Admin> findAdminLogin(String username, String password);
 }
