11220ecddb8045f0f88149935e6f09faf68e3c5a
amelie-quintero <224878763+amelie-quintero@users.noreply.github.com>
2026-03-11 14:42:38 -0400

    added admin login flow

--- src/main/java/com/example/Risto/controller/LoginController.java
+++ src/main/java/com/example/Risto/controller/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Risto.dto.LoginRequestDto;
+import com.example.Risto.services.AdminService;
 import com.example.Risto.services.UserService;
 
 import jakarta.validation.Valid;
@@ -17,8 +18,16 @@ public class LoginController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private AdminService adminService;
+	
 	@PostMapping("/user")
 	public boolean userLogin(@Valid LoginRequestDto data) {
 		return userService.userLogin(data.getUsername(), data.getPassword());
+	}
+	
+	@PostMapping("/admin")
+	public boolean adminLogin(@Valid LoginRequestDto data) {
+		return adminService.adminLogin(data.getUsername(), data.getPassword());
 	}
 }
