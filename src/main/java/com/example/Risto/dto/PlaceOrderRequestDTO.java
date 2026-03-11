38b06432732b7616435e88c6ad2fb8490d40821b
amelie-quintero <224878763+amelie-quintero@users.noreply.github.com>
2026-03-11 15:49:38 -0400

    added DTO's for Place Order

---
+++ src/main/java/com/example/Risto/dto/PlaceOrderRequestDTO.java
@@ -0,0 +1,17 @@
+package com.example.Risto.dto;
+
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class PlaceOrderRequestDTO {
+
+	private int userId;
+	private List<PlaceOrderRequestDishDTO> dishes;
+	
+	@Data
+	private class PlaceOrderRequestDishDTO {
+		private int dishId;
+		private String notes;
+	}
+}
---
+++ src/main/java/com/example/Risto/dto/PlaceOrderResponseDTO.java
@@ -0,0 +1,14 @@
+package com.example.Risto.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class PlaceOrderResponseDTO {
+	private boolean success;
+	private int orderId;
+	private LocalDateTime estimatedReadyAt;
+}
