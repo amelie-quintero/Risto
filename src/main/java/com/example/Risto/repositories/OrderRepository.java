d9e598ff9ed9c7d00c68105c3d9cbd8d1c60286f
amelie-quintero <224878763+amelie-quintero@users.noreply.github.com>
2026-03-12 23:54:09 -0400

    added search for orders by status and date

--- src/main/java/com/example/Risto/repositories/OrderRepository.java
+++ src/main/java/com/example/Risto/repositories/OrderRepository.java
@@ -3,6 +3,7 @@ package com.example.Risto.repositories;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.Risto.constants.OrderStatus;
@@ -13,4 +14,7 @@ public interface OrderRepository extends CrudRepository<Order, Integer> {
 	public List<Order> findByUserId(int userId);
 	public List<Order> findByDateBetween(LocalDateTime startDate, LocalDateTime endDate);
 	public List<Order> findByStatus(OrderStatus status);
+	
+	@Query("Select o from Order o where o.status = ?1 and o.date between ?2 and ?3")
+	public List<Order> findByStatusWithDateBetween(OrderStatus status, LocalDateTime startDate, LocalDateTime endDate);
 }
