7be976678199192043d5d5225ba9188f2668a377
amelie-quintero <224878763+amelie-quintero@users.noreply.github.com>
2026-03-12 22:14:06 -0400

    added method to retrieve full menu

---
+++ src/main/java/com/example/Risto/controller/DishController.java
@@ -0,0 +1,22 @@
+package com.example.Risto.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Risto.dto.MenuResponseDTO;
+import com.example.Risto.services.DishService;
+
+@RestController
+@RequestMapping(value = "/menu")
+public class DishController {
+	
+	@Autowired
+	private DishService dishService;
+	
+	@PostMapping(value = "/all")
+	public MenuResponseDTO getFullMenu() {
+		return MenuResponseDTO.builder().dishes(dishService.getMenu(true)).build();
+	}
+}
